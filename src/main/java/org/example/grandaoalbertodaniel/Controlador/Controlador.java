1dfdf4911e6a48b90cc22e87189d75021105b336
Daniel García <Dagarlow@gmail.com>
2025-02-25 09:48:13 +0100

    MOngo integrado

--- src/main/java/org/example/grandaoalbertodaniel/Controlador/Controlador.java
+++ src/main/java/org/example/grandaoalbertodaniel/Controlador/Controlador.java
@@ -1,45 +1,56 @@
 package org.example.grandaoalbertodaniel.Controlador;
 
+import com.mongodb.client.MongoClient;
 import jakarta.validation.Valid;
 import org.example.grandaoalbertodaniel.DTO.Pelicula;
 import org.example.grandaoalbertodaniel.Interfaces.InterfacesJPA.PeliculaRepository;
 import org.example.grandaoalbertodaniel.Interfaces.InterfacesMongo.PeliculaMongoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/grandao")
 public class Controlador {
 
+    private final MongoClient mongo;
+
+    @Autowired
     PeliculaMongoRepository peliculaMongoRepository;
+
+    @Autowired
     PeliculaRepository peliculaJPARepository;
 
-    public Controlador(PeliculaMongoRepository peliculaMongoRepository, PeliculaRepository peliculaJPARepository) {
+    @Autowired
+    public Controlador(PeliculaMongoRepository peliculaMongoRepository, PeliculaRepository peliculaJPARepository, MongoClient mongo) {
         this.peliculaMongoRepository = peliculaMongoRepository;
         this.peliculaJPARepository = peliculaJPARepository;
+        this.mongo = mongo;
     }
 
     //CONTROLADOR DE MYSQL
 
     @GetMapping("/jpa")
-    public ResponseEntity get() {
+    public ResponseEntity<List<?>> get() {
         return ResponseEntity.ok(peliculaJPARepository.findAll());
     }
 
     @PostMapping("/jpa")
-    public ResponseEntity post(@Valid @RequestBody Pelicula pelicula) {
+    public ResponseEntity<?> post(@Valid @RequestBody Pelicula pelicula) {
         peliculaJPARepository.save(pelicula);
         return ResponseEntity.ok(pelicula);
     }
 
     @PutMapping("/jpa")
-    public ResponseEntity update(@Valid @RequestBody Pelicula pelicula) {
+    public ResponseEntity<?> update(@Valid @RequestBody Pelicula pelicula) {
         peliculaJPARepository.save(pelicula);
         return ResponseEntity.ok(pelicula);
     }
 
     @DeleteMapping("/jpa/{id}")
-    public ResponseEntity delete(@PathVariable Integer id) {
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
         Pelicula pelicula = peliculaJPARepository.findById(id).get();
         peliculaJPARepository.delete(pelicula);
         return ResponseEntity.ok(pelicula);
@@ -47,4 +58,31 @@ public class Controlador {
 
     //CONTROLADOR DE MONGODB
 
+    //Select ALL Películas
+    @GetMapping("/mongo")
+    public ResponseEntity<List<?>> obtenerMongo() {
+        return ResponseEntity.ok(peliculaMongoRepository.findAll());
+    }
+
+    //Crear Película
+    @PostMapping("/mongo")
+    public ResponseEntity<?> guardarMongo(@RequestBody Pelicula pelicula) {
+        peliculaMongoRepository.save(pelicula);
+        return ResponseEntity.ok(pelicula);
+    }
+
+    //Actualizar Película
+    @PutMapping("/mongo")
+    public ResponseEntity<?> updateMongo(@Valid @RequestBody Pelicula pelicula) {
+        peliculaMongoRepository.save(pelicula);
+        return ResponseEntity.ok(pelicula);
+    }
+
+    // Eliminar película
+    @DeleteMapping("/mongo/{id}")
+    public ResponseEntity<?> deleteMongo(@PathVariable Integer id) {
+        Pelicula pelicula = peliculaMongoRepository.findById(id).get();
+        peliculaMongoRepository.delete(pelicula);
+        return ResponseEntity.ok(pelicula);
+    }
 }
--- src/main/java/org/example/grandaoalbertodaniel/Interfaces/InterfacesMongo/PeliculaMongoRepository.java
+++ src/main/java/org/example/grandaoalbertodaniel/Interfaces/InterfacesMongo/PeliculaMongoRepository.java
@@ -3,5 +3,5 @@ package org.example.grandaoalbertodaniel.Interfaces.InterfacesMongo;
 import org.example.grandaoalbertodaniel.DTO.Pelicula;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface PeliculaMongoRepository extends MongoRepository<Pelicula, String> {
+public interface PeliculaMongoRepository extends MongoRepository<Pelicula, Integer> {
 }
